151cf70c8297d7a89ece977b6009583ce213dc19
DouMaokang <alecdou0001@gmail.com>
2021-02-26 17:26:24 +0800

    Created react app

---
+++ UI/search-engine-react/src/App.js
@@ -0,0 +1,12 @@
+import SearchBar from "./components/SearchBar";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
