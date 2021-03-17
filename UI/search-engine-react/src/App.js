559be11120e31e984fde35c7add177f54a4175ea
SUN YETONG <ysun018@e.ntu.edu.sg>
2021-03-17 17:55:55 +0800

    first commit

--- UI/search-engine-react/src/App.js
+++ UI/search-engine-react/src/App.js
@@ -1,10 +1,33 @@
 import SearchBar from "./components/SearchBar";
+import Histogram from "./components/Histogram";
 import React from "react";
+import ReactWordcloud from 'react-wordcloud';
 
 function App() {
+  const words = [
+    {
+      text: 'told',
+      value: 64,
+    },
+    {
+      text: 'mistake',
+      value: 11,
+    },
+    {
+      text: 'thought',
+      value: 16,
+    },
+    {
+      text: 'bad',
+      value: 17,
+    },
+  ]
+  
   return (
     <div>
       <SearchBar />
+      <Histogram />
+      <ReactWordcloud words={words} />
     </div>
   );
 }
